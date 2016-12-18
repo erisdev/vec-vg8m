6cb1b84565bd0e8ce4b6c5aba0317969cca40dc0
eris metis <git@erismetis.name>
2016-12-18 04:25:48 -0500

    have a real boot loader now

--- bios/system.c
+++ bios/system.c
@@ -2,46 +2,126 @@
 #include <string.h>
 #include <hwregs.h>
 
-uint8_t reboot_mode;
+#define EI __asm ei __endasm
+#define DI __asm di __endasm
+#define HALT __asm halt __endasm
 
 extern void int_ignore(void);
-extern uint8_t sin(uint8_t);
 
 typedef void (*int_handler_t)(void);
+static int_handler_t __at(0x0E20) int_vblank;
+static int_handler_t __at(0x0E22) int_hblank;
 
 struct cart_header {
-    uint8_t  magic[8];
-    uint8_t  title[0x20];
-    uint8_t  developer[0x20];
-    uint8_t  language[2];
-    uint16_t flags;
-    uint8_t  version;
-    uint8_t  reserved[0xB3];
+    char  magic[8];
+    char  title[0x20];
+    char  developer[0x20];
+    char  language[2];
+    short flags;
+    char  version;
+    char  reserved0[0x33];
+    short entry_point;
+    char  reserved1[0x7E];
 };
 
-static void int_set(uint8_t intid, int_handler_t handler);
-static void int_clear(uint8_t intid);
+static void wait_frames(char n);
+static void slow_print(char *dst, const char *src);
 
 static void VBLANK(void) __interrupt(0x20);
-static void HBLANK(void) __interrupt(0x22);
+// static void HBLANK(void) __interrupt(0x22);
+
+static struct cart_header __at(0x8000) cart;
+
+static char msg_buffer[96];
+
+static const char msg_blank[]        = "                                ";
+static const char msg_copyright[]    = "(C) 2187 VIDIUS ELECTRIC CORP   ";
+static const char msg_loading[]      = "LOADING...                      ";
+static const char msg_unrecognized[] = "UNRECOGNIZED CARTRIDGE          ";
+static const char msg_bad_entry[]    = "BAD ENTRY POINT                 ";
+static const char msg_bad_checksum[] = "BAD CHECKSUM                    ";
+
+const char palette[] = {0x00, 0x13};
+
+const char fade_to_red[]   = {0x11, 0x0F, 0x02, 0x1A, 0x1C};
+const char fade_to_black[] = {0x11, 0x0F, 0x02, 0x01, 0x00};
 
 void system_main(void) __naked {
-    // static struct cart_header __at(0x8000) cart;
+    int i;
+
+    memcpy(r_palettes[0], palette, sizeof(palette));
+
+    r_txt_addr    = msg_buffer;
+    r_txt_vsize   =   3;
+    r_txt_hsize   =  32;
+    r_txt_vscroll = 240;
+    r_txt_hscroll =   8;
+
+    int_vblank = VBLANK;
+    EI;
+
+    slow_print(msg_buffer, msg_copyright);
+    wait_frames(20);
+
+    slow_print(msg_buffer, msg_loading);
+    wait_frames(20);
+
+    if (strncmp(cart.magic, "VEC-VG8M", sizeof(cart.magic)) != 0) {
+        slow_print(msg_buffer, msg_unrecognized);
+        goto failure;
+    }
+
+    if (cart.entry_point < 0x8000) {
+        slow_print(msg_buffer, msg_bad_entry);
+        goto failure;
+    }
+
+    r_txt_vscroll -= 8;
+    slow_print(msg_buffer + 32, cart.title);
+
+    r_txt_vscroll -= 8;
+    slow_print(msg_buffer + 64, cart.developer);
+
+    slow_print(msg_buffer, msg_blank);
+    wait_frames(60);
+
+    for (i = 0; i < sizeof(fade_to_red); ++i) {
+        r_palettes[0][1] = fade_to_black[i];
+        wait_frames(4);
+    }
+
+    DI;
+    int_vblank = int_ignore;
 
     __asm
-        ei
-        jp      0x8100  ; jump into program
+        ld      hl,(0x8080)
+        jp      (hl)
     __endasm;
+
+failure:
+    for (i = 0; i < sizeof(fade_to_red); ++i) {
+        r_palettes[0][1] = fade_to_red[i];
+        wait_frames(4);
+    }
+
+    for (;;) HALT;
+
 }
 
-/* setting & clearing interrupt handlers */
-
-static int_handler_t __at(0x0E00) int_handlers[0x80];
-
-static void int_set(uint8_t intid, int_handler_t handler) {
-    int_handlers[intid >> 1] = handler;
+volatile char frames_wait;
+static void wait_frames(char n) {
+    frames_wait = n;
+    while (frames_wait) HALT;
 }
 
-static void int_clear(uint8_t intid) {
-    int_handlers[intid >> 1] = int_ignore;
+static void slow_print(char *dst, const char *src) {
+    int i;
+    for (i = 0; i < 32; ++i) {
+        dst[i] = src[i];
+        wait_frames(1);
+    }
+}
+
+static void VBLANK(void) __interrupt(0x20) {
+    if (frames_wait) --frames_wait;
 }
