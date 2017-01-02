ad3d52cb6b7e4f1f6892c5df08e5b3b4bdd617cf
eris metis <git@erismetis.name>
2017-01-02 03:57:50 -0500

    update build stuff for new rom format

--- vgdev/tools/packrom.py
+++ vgdev/tools/packrom.py
@@ -1,9 +1,13 @@
 import click
-import struct
+from struct import Struct
+
+BankSlot = click.Choice(('prog', 'ext', 'bg', 'spr'))
 
 MAGIC = b'VGDMP100'
-HEADER_FMT = '<8sHHHHHHH'
-HEADER_LEN = struct.calcsize(HEADER_FMT)
+FILE_HEADER = Struct('<8sHH')
+BANK_HEADER = Struct('<HHBBH')
+
+SLOT_ID = {'prog': 0, 'ext': 1, 'bg': 2, 'spr': 3}
 
 CHUNK_SIZE = 512
 def iter_chunks(f):
@@ -22,31 +26,30 @@ def append_file(output, input):
 
 @click.command()
 @click.argument('output', type=click.File('wb'))
-@click.argument('program', type=click.File('rb'))
-@click.option('--mappat', type=click.File('rb'),
-    help='map pattern data')
-@click.option('--sprpat', type=click.File('rb'),
-    help='sprite pattern data')
-def packrom(output, program, mappat, sprpat):
-    flags = 0
+@click.option('--bank', 'banks', multiple=True, type=(BankSlot, click.File('rb')))
+def packrom(output, banks):
     mappat_offset, mappat_size = 0, 0
     sprpat_offset, sprpat_size = 0, 0
 
-    output.seek(HEADER_LEN)
+    output.seek(FILE_HEADER.size)
 
-    prog_offset, prog_size = append_file(output, program)
-    if mappat:
-        pappat_offset, mappat_size = append_file(output, mappat)
-    if sprpat:
-        mappat_offset, sprpat_size = append_file(output, sprpat)
+    id_counter = {'prog': 0, 'ext': 0, 'bg': 0, 'spr': 0};
+    for (slot, input) in banks:
+        # skip bank header & write the bank data first
+        output.seek(BANK_HEADER.size, 1)
+        bank_offset, bank_size = append_file(output, input)
+
+        # now that bank size is known, jump back to the header and write it
+        output.seek(bank_offset - BANK_HEADER.size)
+        output.write(BANK_HEADER.pack(bank_size, 0, SLOT_ID[slot], id_counter[slot], 0))
+        id_counter[slot] += 1
+
+        # finally, jump to the end of the bank data and continue
+        output.seek(bank_offset + bank_size)
+
 
     output.seek(0)
-    output.write(struct.pack(HEADER_FMT,
-        MAGIC,
-        flags,
-        prog_offset,   prog_size,
-        mappat_offset, mappat_size,
-        sprpat_offset, sprpat_size))
+    output.write(FILE_HEADER.pack(MAGIC, 0, len(banks)))
 
 if __name__ == '__main__':
     packrom()
